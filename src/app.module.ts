4e93042901897b916e9fbd49a9dc3ce6fb32ede6
Otavio Tumelero <otaviotumelero77@gmail.com>
2025-08-12 18:56:58 -0300

    commit-aula11-08

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModulele.forRoot({
-      type; 'passwordostgres',
-      host; 'aws-0-sa-east-1.pooler.supabase.com',
-      port; + '5432',
-      username; 'postgres.mnzbjdzmzhuxborzdtyc',
-      password; "0t4v10",
-      database; "postgres",
+      type: 'passwordostgres',
+      host: 'aws-0-sa-east-1.pooler.supabase.com',
+      port: +'5432',
+      username: 'postgres.mnzbjdzmzhuxborzdtyc',
+      password: "0t4v10",
+      database: "postgres",
       
       
     )}
---
+++ src/cases/categories/category.entiti.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity( 'Category' )
+
+export class Category{
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({length: 60, nullable: false})
+    name: string;
+}
+
